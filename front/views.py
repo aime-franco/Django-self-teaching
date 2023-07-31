16ed4263836aaf4c222eb3fef68ecaa77a2558f9
endanger <endanger@kali.google.com>
2023-07-31 10:07:02 +0200

    update my work

--- front/views.py
+++ front/views.py
@@ -1,27 +1,54 @@
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Room, Topic
-
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .forms import RoomForm
 # Create your views here.
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User doesn\'t exist')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username OR password is invalid')
+    content = {}        
+    return render(request, 'front/login_register.html', content)
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
 
 def home(request):
     q = request.GET.get('p') if request.GET.get('p') != None else ''
-    rooms = Room.objects.filter(Q(topic__name__icontains=q) &
-    Q(name__icontains=q) |
-    Q(description__icontains=q) |
-    Q(description__icontains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+    Q(name__icontains=q)
     )
     topics = Topic.objects.all() 
-    context = {'rooms': rooms, 'topics': topics}
+    count_room = rooms.count() # count the number of rooms the topic
+    context = {'rooms': rooms, 'topics': topics, 'count_room': count_room}
     return render(request, 'front/home.html', context)
 
+@login_required(login_url='login')
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'front/room.html', context) 
 
+@login_required(login_url='login')
 def create_room(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -33,6 +60,7 @@ def create_room(request):
     context = {'form': form}
     return render(request, 'front/room_form.html', context)
 
+@login_required(login_url='login')
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -44,6 +72,8 @@ def update_room(request, pk):
     content = {'form': form}
     return render(request, 'front/room_form.html', content)
 
+
+@login_required(login_url='login')
 def delete_room(request, pk):
     room = Room.objects.get(id=pk)
     
